4e14239e7e12c146a6d3e334a275815d1bba54c8
Sef <Sef@reborn.com>
2019-02-13 02:45:38 +0100

    begin

---
+++ src/Competitie.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Competitie {
+
+    // Instance fields
+    private String naam;
+    private ArrayList<Wedstrijd> wedstrijden = new ArrayList<Wedstrijd>();
+    private ArrayList<Integer> nums = new ArrayList<Integer>();
+    private ArrayList<Team> teams;
+
+}
